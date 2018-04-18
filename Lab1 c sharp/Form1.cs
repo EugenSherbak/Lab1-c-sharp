edcc0dee3287109715cf49d4ef7cd851b06f8f51
Eugen Sherbak <sherbak13579@gmail.com>
2018-04-18 22:10:38 +0300

    Little bit changes
    Add try-catch

--- Lab1 c sharp/Form1.cs
+++ Lab1 c sharp/Form1.cs
@@ -23,21 +23,28 @@ namespace Lab1_c_sharp
 
         private void CalcButton_Click(object sender, EventArgs e)
         {
-            vector1 = new Vector(Convert.ToDouble(textBoX1.Text == "" ? "Error" : textBoX1.Text),
-                Convert.ToDouble(textBoY1.Text == "" ? "Error" : textBoY1.Text),
-                Convert.ToDouble(textBoZ1.Text == "" ? "Error" : textBoZ1.Text));
-            vector2 = new Vector(Convert.ToDouble(textBoX2.Text == "" ? "Error" : textBoX2.Text),
-                Convert.ToDouble(textBoY2.Text == "" ? "Error" : textBoY2.Text),
-                Convert.ToDouble(textBoZ2.Text == "" ? "Error" : textBoZ2.Text));
-            vector3 = new Vector(Convert.ToDouble(textBoX3.Text == "" ? "Error" : textBoX3.Text),
-                Convert.ToDouble(textBoY3.Text == "" ? "Error" : textBoY3.Text),
-                Convert.ToDouble(textBoZ3.Text == "" ? "Error" : textBoZ3.Text));
-            Vector r = (vector1 * vector2);
-            r *= 2;
-            Vector d = (vector1 + vector3) * r;
+            try
+            {
+                vector1 = new Vector(Convert.ToDouble(textBoX1.Text/* == "" ? "0" : textBoX1.Text*/),
+                    Convert.ToDouble(textBoY1.Text/* == "" ? "0" : textBoY1.Text*/),
+                    Convert.ToDouble(textBoZ1.Text/* == "" ? "0" : textBoZ1.Text*/));
+                vector2 = new Vector(Convert.ToDouble(textBoX2.Text/* == "" ? "0" : textBoX2.Text*/),
+                    Convert.ToDouble(textBoY2.Text/* == "" ? "0" : textBoY2.Text*/),
+                    Convert.ToDouble(textBoZ2.Text/* == "" ? "0" : textBoZ2.Text*/));
+                vector3 = new Vector(Convert.ToDouble(textBoX3.Text/* == "" ? "0" : textBoX3.Text*/),
+                    Convert.ToDouble(textBoY3.Text/* == "" ? "0" : textBoY3.Text*/),
+                    Convert.ToDouble(textBoZ3.Text/* == "" ? "0" : textBoZ3.Text*/));
+                Vector r = (vector1 * vector2);
+                r *= 2;
+                Vector d = (vector1 + vector3) * r;
+                richTextBoxVector.Text = vector1.ToString() + "\n" + vector2.ToString() + "\n" + vector3.ToString() +
+    "\nr = 2 * (a + b) x c;\tr = " + r.ScalarProduct(r, vector3) + "\nd = (a + c) * r;\td = " + d;
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели не все координаты или же ввели их некорректно. Повторите попытку");
+            }
 
-            richTextBoxVector.Text = vector1.ToString() + "\n" + vector2.ToString() + "\n" + vector3.ToString() +
-                "\nr = 2 * (a + b) x c;\tr = " + r.ScalarProduct(r, vector3) + "\nd = (a + c) * r;\td = " + d; 
         }
     }
 }
